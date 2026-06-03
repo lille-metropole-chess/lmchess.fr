91afe18021ff59606b84e0f822734843b0a369c2
nicolasey <nicolas.sey@gmail.com>
2026-06-03 14:44:04 +0200

    adding basics

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,23 @@ import markdoc from '@astrojs/markdoc';
 import node from '@astrojs/node';
 import keystatic from '@keystatic/astro';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import partytown from '@astrojs/partytown';
+
+import sitemap from '@astrojs/sitemap';
+
+import mailObfuscation from 'astro-mail-obfuscation';
+
+import robotsTxt from 'astro-robots-txt';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: node({ mode: 'standalone' }),
-  integrations: [react(), markdoc(), keystatic()],
+  integrations: [react(), markdoc(), keystatic(), partytown(), sitemap(), mailObfuscation(), robotsTxt()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
