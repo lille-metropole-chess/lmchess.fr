14c113f8f870ba508e3795836664ed580dbbf8e4
Pham Gia Thuy <gtpham@MacBook-Air-de-Pham.local>
2026-06-05 19:27:48 +0200

    Add first test

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,4 +1,5 @@
 
 export default new Map([
-["src/content/posts/first-post.mdoc", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Ffirst-post.mdoc&astroContentModuleFlag=true")]]);
+["src/content/posts/first-post.mdoc", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Ffirst-post.mdoc&astroContentModuleFlag=true")],
+["src/content/posts/test.mdoc", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Ftest.mdoc&astroContentModuleFlag=true")]]);
 		
